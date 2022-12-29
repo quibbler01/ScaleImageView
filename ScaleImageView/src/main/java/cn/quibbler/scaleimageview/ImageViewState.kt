68a4af60383f1ede14d6ad8573b44535e49b48d2
quibbler01 <fluent0418@gmail.com>
2022-12-29 21:53:14 +0800

    complete class ImageViewState {

--- ScaleImageView/src/main/java/cn/quibbler/scaleimageview/ImageViewState.kt
+++ ScaleImageView/src/main/java/cn/quibbler/scaleimageview/ImageViewState.kt
@@ -1,4 +1,28 @@
 package cn.quibbler.scaleimageview
 
-class ImageViewState {
+import android.graphics.PointF
+import java.io.Serializable
+
+/**
+ * Wraps the scale, center and orientation of a displayed image for easy restoration on screen rotate.
+ */
+class ImageViewState : Serializable {
+
+    val scale: Float
+
+    private val centerX: Float
+
+    private val centerY: Float
+
+    val orientation: Int
+
+    constructor(scale: Float, center: PointF, orientation: Int) {
+        this.scale = scale
+        this.centerX = center.x
+        this.centerY = center.y
+        this.orientation = orientation
+    }
+
+    fun getCenter(): PointF = PointF(centerX, centerY)
+
 }
