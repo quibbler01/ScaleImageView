2dbf24807bfc83006c0f4b66bd5d34d5387063b6
quibbler01 <fluent0418@gmail.com>
2022-12-30 00:01:18 +0800

    create core class SubsamplingScaleImageView

--- ScaleImageView/src/main/java/cn/quibbler/scaleimageview/SubsamplingScaleImageView.kt
+++ ScaleImageView/src/main/java/cn/quibbler/scaleimageview/SubsamplingScaleImageView.kt
@@ -4,6 +4,23 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 
+/**
+ * <p>
+ * Displays an image subsampled as necessary to avoid loading too much image data into memory. After zooming in,
+ * a set of image tiles subsampled at higher resolution are loaded and displayed over the base layer. During pan and
+ * zoom, tiles off screen or higher/lower resolution than required are discarded from memory.
+ * </p><p>
+ * Tiles are no larger than the max supported bitmap size, so with large images tiling may be used even when zoomed out.
+ * </p><p>
+ * v prefixes - coordinates, translations and distances measured in screen (view) pixels
+ * <br>
+ * s prefixes - coordinates, translations and distances measured in rotated and cropped source image pixels (scaled)
+ * <br>
+ * f prefixes - coordinates, translations and distances measured in original unrotated, uncropped source file pixels
+ * </p><p>
+ * <a href="https://github.com/davemorrissey/subsampling-scale-image-view">View project on GitHub</a>
+ * </p>
+ */
 class SubsamplingScaleImageView : View {
 
     constructor(context: Context?) : super(context)
