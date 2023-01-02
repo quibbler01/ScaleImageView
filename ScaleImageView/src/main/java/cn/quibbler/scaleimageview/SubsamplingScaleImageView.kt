0ea9000cf601298c4e092059ad1417768523c4d1
quibbler01 <fluent0418@gmail.com>
2023-01-02 13:58:36 +0800

    develop SubsamplingScaleImageView functions

--- ScaleImageView/src/main/java/cn/quibbler/scaleimageview/SubsamplingScaleImageView.kt
+++ ScaleImageView/src/main/java/cn/quibbler/scaleimageview/SubsamplingScaleImageView.kt
@@ -2,18 +2,23 @@ package cn.quibbler.scaleimageview
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.PointF
-import android.graphics.Rect
+import android.graphics.*
 import android.net.Uri
 import android.os.AsyncTask
+import android.os.Handler
 import android.util.AttributeSet
+import android.util.Log
 import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
+import androidx.annotation.AnyThread
+import androidx.core.view.ContentInfoCompat.Flags
 import cn.quibbler.scaleimageview.decoder.*
 import java.util.*
 import java.util.concurrent.locks.ReadWriteLock
 import java.util.concurrent.locks.ReentrantReadWriteLock
+import kotlin.math.abs
+import kotlin.math.min
 
 /**
  * <p>
@@ -113,12 +118,12 @@ class SubsamplingScaleImageView : View {
 
         // overrides for the dimensions of the generated tiles
         const val TILE_SIZE_AUTO = Int.MAX_VALUE
-    }
 
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+        private const val MESSAGE_LONG_CLICK = 1
+
+        // A global preference for bitmap format, available to decoder classes that respect it
+        private var preferredBitmapConfig: Bitmap.Config? = null
+    }
 
     // Bitmap (preview or full image)
     private var bitmap: Bitmap? = null
@@ -144,7 +149,7 @@ class SubsamplingScaleImageView : View {
     private var orientation = ORIENTATION_0
 
     // Max scale allowed (prevent infinite zoom)
-    private var maxScale = 2f
+    private var maxScale: Float = 2f
 
     // Min scale allowed (prevent infinite zoom)
     private var minScale: Float = minScale()
@@ -236,17 +241,184 @@ class SubsamplingScaleImageView : View {
     // Scale and center animation tracking
     private var anim: Anim? = null
 
+    // Whether a ready notification has been sent to subclasses
+    private var readySent = false
+
+    // Whether a base layer loaded notification has been sent to subclasses
+    private var imageLoadedSent = false
+
+    // Event listener
+    private var onImageEventListener: OnImageEventListener? = null
+
+    // Scale and center listener
+    private var onStateChangedListener: OnStateChangedListener? = null
+
+    // Long click listener
+    private var onLongClickListener: OnLongClickListener? = null
+
+    // Long click handler
+    private val handler: Handler
+
+    // Paint objects created once and reused for efficiency
+    private var bitmapPaint: Paint? = null
+    private var debugTextPaint: Paint? = null
+    private var debugLinePaint: Paint? = null
+    private var tileBgPaint: Paint? = null
+
+    // Volatile fields used to reduce object creation
+    private var satTemp: ScaleAndTranslate? = null
+    private var matrix: Matrix? = null
+    private var sRect: RectF? = null
+    private val srcArray = FloatArray(8)
+    private val dstArray = FloatArray(8)
+
+    //The logical density of the display
+    private val density: Float
+
+    constructor(context: Context?) : this(context, null)
+    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, defStyleAttr, 0)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
+        density = resources.displayMetrics.density
+        setMinimumDpi(160)
+        setDoubleTapZoomDpi(160)
+        setMinimumTileDpi(320)
+    }
+
+    /**
+     * Reset all state before setting/changing image or setting new rotation.
+     */
+    private fun reset(newImage: Boolean) {
+        debug("reset newImage = $newImage")
+        scale = 0f
+        scaleStart = 0f
+        vTranslate = null
+        vTranslateStart = null
+        vTranslateBefore = null
+        pendingScale = 0f
+        sPendingCenter = null
+        sRequestedCenter = null
+        isZooming = false
+        isPanning = false
+        isQuickScaling = false
+        maxTouchCount = 0
+        fullImageSampleSize = 0
+        vCenterStart = null
+        vDistStart = 0f
+        quickScaleLastDistance = 0f
+        quickScaleMoved = false
+        quickScaleSCenter = null
+        quickScaleVLastPoint = null
+        quickScaleVStart = null
+        anim = null
+        satTemp = null
+        matrix = null
+        sRect = null
+        if (newImage) {
+            uri = null
+            decoderLock.writeLock().lock()
+            try {
+                decoder?.recycle()
+                decoder = null
+            } finally {
+                decoderLock.writeLock().unlock()
+            }
+            if (!bitmapIsCached) {
+                bitmap?.recycle()
+            }
+            if (bitmap != null && bitmapIsCached) {
+                onImageEventListener?.onPreviewReleased()
+            }
+            sWidth = 0
+            sHeight = 0
+            sOrientation = 0
+            sRegion = null
+            pRegion = null
+            readySent = false
+            imageLoadedSent = false
+            bitmap = null
+            bitmapIsPreview = false
+            bitmapIsCached = false
+        }
+        tileMap?.let {
+            for (tileMapEntry in it.entries) {
+                for (tile in tileMapEntry.value) {
+                    tile.visible = false
+                    tile.bitmap?.recycle()
+                    tile.bitmap = null
+                }
+            }
+        }
+        tileMap = null
+        setGestureDetector(context)
+    }
+
+    private fun setGestureDetector(context: Context) {
+        this.detector = GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+                if (panEnabled && readySent && vTranslate != null && e1 != null && e2 != null && (abs(e1.x - e2.x) > 50 || abs(e1.y - e2.y) > 50) && (abs(velocityX) > 500 || abs(velocityY) > 500) && !isZooming) {
+                    val vTranslateEnd: PointF = PointF(vTranslate!!.x + velocityX * 0.25f, vTranslate!!.y + velocityY * 0.25f)
+                    val sCenterXEnd: Float = (width / 2 - vTranslateEnd.x / scale)
+                    val sCenterYEnd: Float = (height / 2 - vTranslateEnd.y) / scale
+                    AnimationBuilder(PointF(sCenterXEnd, sCenterYEnd)).withEasing(EASE_OUT_QUAD).withPanLimited(false).withOrigin(ORIGIN_FLING).start()
+                    return true
+                }
+                return super.onFling(e1, e2, velocityX, velocityY)
+            }
+
+            override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
+                performClick()
+                return true
+            }
+
+            override fun onDoubleTap(e: MotionEvent): Boolean {
+                if (zoomEnabled && readySent && vTranslate != null) {
+                    // Hacky solution for #15 - after a double tap the GestureDetector gets in a state
+                    // where the next fling is ignored, so here we replace it with a new one.
+                    setGestureDetector(context)
+                    if (quickScaleEnabled) {
+                        // Store quick scale params. This will become either a double tap zoom or a
+                        // quick scale depending on whether the user swipes.
+                        vCenterStart = PointF(e.x, e.y)
+                        vTranslateStart = PointF(vTranslate!!.x, vTranslate!!.y)
+                        scaleStart = scale
+                        isQuickScaling = true
+                        isZooming = true
+                        quickScaleLastDistance = -1f
+                        quickScaleSCenter = viewToSourceCoord(vCenterStart!!)
+                        quickScaleVStart = PointF(e.x, e.y)
+                        quickScaleVLastPoint = PointF(quickScaleSCenter.x, quickScaleSCenter.y)
+                        quickScaleMoved = false
+                        // We need to get events in onTouchEvent after this.
+                        return false
+                    } else {
+                        // Start double tap zoom animation.
+                        doubleTapZoom(viewToSourceCoord(PointF(e.x, e.y)), PointF(e.x, e.y))
+                        return true
+                    }
+                }
+                return super.onDoubleTap(e)
+            }
+        })
+
+        singleDetector = GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
+                performClick()
+                return true
+            }
+        })
+    }
 
     private class Tile {
-        private var sRect: Rect? = null
-        private var sampleSize = 0
-        private var bitmap: Bitmap? = null
-        private var loading = false
-        private var visible = false
+        var sRect: Rect? = null
+        var sampleSize = 0
+        var bitmap: Bitmap? = null
+        var loading = false
+        var visible = false
 
         // Volatile fields instantiated once then updated before use to reduce GC.
-        private var vRect: Rect? = null
-        private var fileSRect: Rect? = null
+        var vRect: Rect? = null
+        var fileSRect: Rect? = null
     }
 
     private class Anim {
@@ -302,5 +474,106 @@ class SubsamplingScaleImageView : View {
         fun onInterruptedByNewAnim()
     }
 
+    /**
+     * This is a screen density aware alternative to {@link #setMaxScale(float)}; it allows you to express the maximum
+     * allowed scale in terms of the minimum pixel density. This avoids the problem of 1:1 scale still being
+     * too small on a high density screen. A sensible starting point is 160 - the default used by this view.
+     * @param dpi Source image pixel density at maximum zoom.
+     */
+    fun setMinimumDpi(dpi: Int) {
+        val metrics = resources.displayMetrics
+        val averageDpi: Float = (metrics.xdpi + metrics.ydpi) / 2.0f
+        maxScale = averageDpi / dpi
+    }
+
+    /**
+     * A density aware alternative to {@link #setDoubleTapZoomScale(float)}; this allows you to express the scale the
+     * image will zoom in to when double tapped in terms of the image pixel density. Values lower than the max scale will
+     * be ignored. A sensible starting point is 160 - the default used by this view.
+     * @param dpi New value for double tap gesture zoom scale.
+     */
+    fun setDoubleTapZoomDpi(dpi: Int) {
+        val metrics = resources.displayMetrics
+        val averageDpi: Float = (metrics.xdpi + metrics.ydpi) / 2
+        doubleTapZoomScale = averageDpi / dpi
+    }
+
+    /**
+     * By default, image tiles are at least as high resolution as the screen. For a retina screen this may not be
+     * necessary, and may increase the likelihood of an OutOfMemoryError. This method sets a DPI at which higher
+     * resolution tiles should be loaded. Using a lower number will on average use less memory but result in a lower
+     * quality image. 160-240dpi will usually be enough. This should be called before setting the image source,
+     * because it affects which tiles get loaded. When using an untiled source image this method has no effect.
+     * @param minimumTileDpi Tile loading threshold.
+     */
+    fun setMinimumTileDpi(minimumTileDpi: Int) {
+        val metrics = resources.displayMetrics
+        val averageDpi: Float = (metrics.xdpi + metrics.ydpi) / 2
+        this.minimumTileDpiv = min(averageDpi, minimumTileDpi.toFloat()).toInt()
+        if (readySent) {
+            reset(false)
+            invalidate()
+        }
+    }
+
+    /**
+     * Convert screen coordinate to source coordinate.
+     * @param vxy view X/Y coordinate.
+     * @return a coordinate representing the corresponding source coordinate.
+     */
+    fun viewToSourceCoord(vxy: PointF): PointF? {
+        return viewToSourceCoord(vxy.x, vxy.y, PointF())
+    }
+
+    /**
+     * Convert screen to source x coordinate.
+     */
+    private fun viewToSourceX(vx: Float): Float {
+        if (vTranslate == null) return Float.NaN
+        return (vx - vTranslate!!.x) / scale
+    }
+
+    /**
+     * Convert screen to source y coordinate.
+     */
+    private fun viewToSourceY(vy: Float): Float {
+        if (vTranslate == null) return Float.NaN
+        return (vy - vTranslate!!.y) / scale
+    }
+
+    fun viewToSourceCoord(vx: Float, vy: Float, sTarget: PointF): PointF? {
+        if (vTranslate == null) {
+            return null
+        }
+        sTarget.set(viewToSourceX(vx), viewToSourceY(vy))
+        return sTarget
+    }
+
+    /**
+     * Convert source to view x coordinate.
+     */
+    private fun sourceToViewX(sx: Float): Float {
+        if (vTranslate == null) return Float.NaN
+        return sx * scale + vTranslate!!.x
+    }
+
+    /**
+     * Convert source to view y coordinate.
+     */
+    private fun sourceToView(sy: Float): Float {
+        if (vTranslate == null) return Float.NaN
+        return sy * scale + vTranslate!!.y
+    }
+
+
+    /**
+     * Debug logger
+     */
+    @AnyThread
+    private fun debug(message: String, vararg args: Any?) {
+        if (debug) {
+            Log.d(TAG, String.format(message, args))
+        }
+    }
 
 }
