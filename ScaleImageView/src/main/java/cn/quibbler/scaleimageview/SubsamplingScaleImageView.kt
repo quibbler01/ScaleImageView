e7167b7601a2a26086618279e6ae824b695d90cf
quibbler01 <fluent0418@gmail.com>
2023-01-07 09:37:21 +0800

    add OnStateChangedListener

--- ScaleImageView/src/main/java/cn/quibbler/scaleimageview/SubsamplingScaleImageView.kt
+++ ScaleImageView/src/main/java/cn/quibbler/scaleimageview/SubsamplingScaleImageView.kt
@@ -1043,6 +1043,30 @@ class SubsamplingScaleImageView : View {
     }
 
     /**
+     * An event listener, allowing activities to be notified of pan and zoom events. Initialisation
+     * and calls made by your code do not trigger events; touch events and animations do. Methods in
+     * this listener will be called on the UI thread and may be called very frequently - your
+     * implementation should return quickly.
+     */
+    interface OnStateChangedListener {
+
+        /**
+         * The scale has changed. Use with {@link #getMaxScale()} and {@link #getMinScale()} to determine
+         * whether the image is fully zoomed in or out.
+         * @param newScale The new scale.
+         * @param origin Where the event originated from - one of {@link #ORIGIN_ANIM}, {@link #ORIGIN_TOUCH}.
+         */
+        fun onScaleChanged(newScale: Float, origin: Int)
+
+        /**
+         * The source center has been changed. This can be a result of panning or zooming.
+         * @param newCenter The new source center point.
+         * @param origin Where the event originated from - one of {@link #ORIGIN_ANIM}, {@link #ORIGIN_TOUCH}.
+         */
+        fun onCenterChanged(newCenter: PointF, origin: Int)
+    }
+
+    /**
      * An event listener, allowing subclasses and activities to be notified of significant events.
      */
     public interface OnImageEventListener {
